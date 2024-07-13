7a4e232c70b4c3ae5203067b7c9154d40055d1e5
Jenish Kothari <kotharijenish2001@gmail.com>
2024-07-13 18:27:16 -0400

    Update in the Application Start Script.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,15 +1,24 @@
+#!/bin/bash
+
+# Set permissions
 sudo chmod -R 777 /home/ec2-user/express-app
 
-#navigate into our working directory where we have all our github files
+# Navigate into our working directory where we have all our GitHub files
 cd /home/ec2-user/express-app
 
-#add npm and node to path
+# Add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#install node modules
+# Install pnpm globally if not installed
+if ! command -v pnpm &> /dev/null
+then
+    npm install -g pnpm
+fi
+
+# Install node modules
 pnpm install
 
-#start our node app in the background
+# Start our node app in the background
 pnpm run dev > app.out.log 2> app.err.log < /dev/null & 
